bc1f65618da38d597750f03d33ba164f0e627c80
Bruno Queguiner <bruno.queguiner@seenovate.com>
2023-09-21 11:21:57 +0200

    :sparkles: Write table boond_table into df

--- load_data.py
+++ load_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from db import connect_to_db, disconnect_from_db, execute_sql_request
+from db import connect_to_db, disconnect_from_db, insert_df_to_table
 from config import logger
 
 
@@ -14,64 +14,70 @@ def execute_script(python_script: str) -> None:
         logger.error(f"Error executing {python_script}: {str(e)}")
 
 
-# # Créer les fichiers CSV nécessaires avec l'API Boond
-# script_to_execute = ['load_projet.py', 'load_profil.py', 'load_crmsociete.py', 'load_achat.py', 'load_projet_achat.py',
-#                      'load_bondecommande.py']
-# for script in script_to_execute:
-#     execute_script('ods/' + script)
+# Créer les fichiers CSV nécessaires avec l'API Boond
+script_to_execute = ['load_projet.py', 'load_profil.py', 'load_crmsociete.py', 'load_achat.py', 'load_projet_achat.py',
+                     'load_bondecommande.py']
+for script in script_to_execute:
+    execute_script('ods/' + script)
 
 # Charger les fichiers CSV
-projet = pd.read_csv("data/projet.csv", sep=",", quotechar='"', index_col=0)
-profil = pd.read_csv("data/profil.csv", sep=",", quotechar='"', index_col=0)
-crmsociete = pd.read_csv("data/crmsociete.csv", sep=",", quotechar='"', index_col=0)
-achat = pd.read_csv("data/data_rfo/achat.csv", sep=";", quotechar='"')
-print(achat)
-projet_achat = pd.read_csv("data/projet_achat.csv", sep=",", quotechar='"', index_col=0)
-bdc = pd.read_csv("data/bondecommande.csv", sep=",", quotechar='"', index_col=0)
+df_projet = pd.read_csv("data/projet.csv", sep=",", quotechar='"', index_col=0)
+df_profil = pd.read_csv("data/profil.csv", sep=",", quotechar='"', index_col=0)
+df_crmsociete = pd.read_csv("data/crmsociete.csv", sep=",", quotechar='"', index_col=0)
+df_achat = pd.read_csv("data/data_rfo/achat.csv", sep=";", quotechar='"')
+df_projet_achat = pd.read_csv("data/projet_achat.csv", sep=",", quotechar='"', index_col=0)
+df_bdc = pd.read_csv("data/bondecommande.csv", sep=",", quotechar='"', index_col=0)
 
-# Garder uniquement l'id et le trigramme dans le profil
-profil['profil_trigramme'] = profil['profil_prenom'].str[0] + profil['profil_nom'].str[:2]
-profil = profil[['id_profil', 'profil_trigramme']]
 
-# Garder les enregistrements uniques dans achat, projet_achat et bdc
-achat = achat.drop_duplicates()
-print(achat)
-print(achat.columns)
-print(len(achat))
-projet_achat = projet_achat.drop_duplicates()
-df_achat = pd.merge(achat, projet_achat, on='id_projet', how='left')
-print(df_achat)
-print(df_achat.columns)
-print(len(df_achat))
-
-bdc = bdc.drop_duplicates()
-
-# Trier bdc par ordre décroissant de id_bondecommande et garder les premiers enregistrements pour chaque id_projet
-bdc = bdc.sort_values(by='id_bondecommande', ascending=False)
-derniers_bdc = bdc.groupby('id_projet').first().reset_index()
-
+# PROJET
+logger.debug("PROJET")
 # Filtrer les lignes dans projet où prj_etat est égal à 1 et prj_typeref est dans [6, 12]
-projet = projet[(projet['prj_etat'] == 1) & (projet['prj_typeref'].isin([6, 12]))]
+df_boond = df_projet[(df_projet['prj_etat'] == 1) & (df_projet['prj_typeref'].isin([6, 12]))]
+logger.debug(f"df_boond.shape = {df_boond.shape}")
 
-# Fusionner projet avec crmsociete sur la colonne 'id_crmsociete' (équivalent d'un LEFT JOIN en SQL)
-projet_update = pd.merge(projet, crmsociete, on='id_crmsociete', how='left')
+# CRM SOCIETE
+logger.debug("CRM SOCIETE")
+df_boond = pd.merge(df_boond, df_crmsociete, on='id_crmsociete', how='left')
+logger.debug(f"df_boond.shape = {df_boond.shape}")
 
-# Fusionner projet_update avec profil sur la colonne 'id_profilcdp' et 'id_profil' (équivalent d'un LEFT JOIN en SQL)
-projet_update = pd.merge(projet_update, profil, left_on='id_profilcdp', right_on='id_profil', how='left')
-
-# Convertir la colonne 'achat_date' en date et filtrer les enregistrements où la date est supérieure à '2022-09-01'
-achat['achat_date'] = pd.to_datetime(achat['achat_date'])
-achat_update = achat[achat['achat_date'] > '2022-09-01']
-
-# Fusionner projet_update avec achat_update sur la colonne 'id_projet' (équivalent d'un LEFT JOIN en SQL)
-projet_achat = pd.merge(projet_update, achat_update, on='id_projet', how='left')
-
+# BON DE COMMANDE
+logger.debug("BON DE COMMANDE")
+df_bdc = df_bdc.drop_duplicates()
+# Trier bdc par ordre décroissant de id_bondecommande et garder les premiers enregistrements pour chaque id_projet
+df_bdc = df_bdc.sort_values(by='id_bondecommande', ascending=False)
+df_bdc = df_bdc.groupby('id_projet').first().reset_index()
 # Fusionner projet_achat avec derniers_bdc sur la colonne 'id_projet' (équivalent d'un LEFT JOIN en SQL)
-projet_achat_bdc = pd.merge(projet_achat, derniers_bdc, on='id_projet', how='left')
+df_boond = pd.merge(df_boond, df_bdc, on='id_projet', how='left')
+logger.debug(f"df_boond.shape = {df_boond.shape}")
+
+# PROFIL
+logger.debug("PROFIL")
+# Garder uniquement l'id et le trigramme dans le profil
+df_profil['profil_trigramme'] = df_profil['profil_prenom'].str[0] + df_profil['profil_nom'].str[:2]
+df_profil = df_profil[['id_profil', 'profil_trigramme']]
+# Fusionner projet_update avec profil sur la colonne 'id_profilcdp' et 'id_profil' (équivalent d'un LEFT JOIN en SQL)
+df_boond = pd.merge(df_boond, df_profil, left_on='id_profilcdp', right_on='id_profil', how='left')
+logger.debug(f"df_boond.shape = {df_boond.shape}")
+
+# PROJET ACHAT
+logger.debug("PROJET ACHAT")
+df_projet_achat = df_projet_achat.drop_duplicates()
+df_boond = pd.merge(df_boond, df_projet_achat, on='id_projet', how='left')
+logger.debug(f"df_boond.shape = {df_boond.shape}")
+
+# ACHAT
+logger.debug("ACHAT")
+df_achat = df_achat.drop_duplicates()
+# Convertir la colonne 'achat_date' en date et prendre le dernier achat en date sur chaque projet
+df_achat['achat_date'] = pd.to_datetime(df_achat['achat_date'])
+df_boond = pd.merge(df_boond, df_achat, on='id_achat', how='left')
+df_boond = df_boond.sort_values(by=['id_projet', 'achat_date'], ascending=[True, False])
+df_boond = df_boond.drop_duplicates(subset='id_projet', keep='first')
+logger.debug(f"df_boond.shape = {df_boond.shape}")
 
 # Écrire le résultat dans un fichier CSV
-projet_achat_bdc.to_csv('./data/pcoe.csv', index=False, sep=';')
+df_boond.to_csv('./data/boond.csv', index=False, sep=';')
 
 conn = connect_to_db()
-res = execute_sql_request("SELECT * from monitoring")
+insert_df_to_table(df_boond, "boond_table", 'replace', conn)
 disconnect_from_db(conn)
